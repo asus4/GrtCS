82b997ed510e23064240838730787a018e27fa6c
Koki Ibukuro <koki.ibukuro@gmail.com>
2018-04-26 22:15:22 +0900

    Add Test of VectorUnitTest class

---
+++ Assets/Grt/Editor/Tests/DataStructures/VectorUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace GRT
+{
+    public static class VectorUnitTest
+    {
+        [Test]
+        public static void DefaultConstructor()
+        {
+            var vec = new Vector<int>();
+            Assert.AreEqual(0, vec.GetSize());
+        }
+
+        [Test]
+        public static void ResizeConstructor()
+        {
+            const uint size = 100;
+            var vec = new Vector<int>(size);
+            Assert.AreEqual(size, vec.GetSize());
+        }
+
+        [Test]
+        public static void CopyConstructor()
+        {
+            const uint size = 100;
+            var vec1 = new Vector<int>(size);
+            Assert.AreEqual(size, vec1.GetSize());
+
+            var vec2 = new Vector<int>(vec1);
+            Assert.AreEqual(vec1.GetSize(), vec2.GetSize());
+        }
+
+        public static void EqualsConstructor()
+        {
+            // Not implemnted since C# can't have "=" overload
+        }
+    }
+}
